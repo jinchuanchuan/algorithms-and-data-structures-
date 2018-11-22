77b30a0583f9d3ab61e60f847e068d51077d065a
jinchuanchuan <sdzzjcc@163.com>
2018-11-22 11:02:37 +0800

    init

---
+++ src/main/java/com/jcc/algorithmsdatastructures/AlgorithmsDataStructuresApplication.java
@@ -0,0 +1,12 @@
+package com.jcc.algorithmsdatastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsDataStructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsDataStructuresApplication.class, args);
+	}
+}
---
+++ src/main/java/exercise40/alg1and5.java
@@ -0,0 +1,10 @@
+package exercise40;
+
+/**
+ * @description:
+ * @author: jcc
+ * @date: 2018-11-22 10:54
+ * @Modified By:
+ */
+public class alg1and5 {
+}
